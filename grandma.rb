74b5618ba8cd20956c0959e624994f725f80e554
Dario Carlino <baires82@gmail.com>
2020-08-18 23:40:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string.upcase == false
+  elsif string == string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
